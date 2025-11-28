24851e56d1c094b371ef7fcc31e9768c15ac5fd3
connorwoods99 <conknar99@outlook.com>
2025-11-28 16:14:56 +0000

    Password encrypter
    Simple password encryption and decryption using ascii

--- Main.cpp
+++ Main.cpp
@@ -1,95 +1,55 @@
-#include <iostream>
-#include <string>
-#include <list>
+#include<iostream>
+#include<string>
 using namespace std;
-//used to check if any of the values in two arrays are the same
-bool anyinarray(const string arr[], int arrsize,
-	const string ref[], int refsize)
-{
-	for (int i = 0; i < arrsize; i++)
-	{
-		for (int j = 0; j < refsize; j++)
-		{
-			if (arr[i] == ref[j]) 
-			{
-				return true;
-			}
-		}
-		
-	}
-	return false;
-}
 
-//used to print an array, loops through all the values of an array and prints them when called
-//void so it can be called anywhere
-void printarray(string r[], int size)
+void encrypt(string P)//converts to ascii
 {
-	for (int i = 0; i < size; i++) {
-		cout << r[i] << " "<<endl;
+	for (int i = 0; i < P.length(); i++) {
+
+		cout << (int)P[i] ;//*3 -12 +55;
+
 	}
-	cout << endl;
+}
+void decrypt(string D, int len)//converts back from ascii to strong
+{
+	int number = 0;
+	for (int j = 0; j < len; j++)
+	{
+		number = number*10 +(D[j]-'0');
+		if (number >= 32 && number <= 122) {
+			char ch = (char)number ; //-55 +12 /3;
+			cout << ch;
+			number = 0;
+		}
+		//cout << (char)D[j] -55 +12 /3 << endl;
+	}
 }
 int main()
-{	//defines ing (used for user input) and its size is kept open so its size can be called 
-	const int size = 3;
-	string ing[size];
-
-	//define recipies and size
-	string rpho[] = { "chicken","broth","noodles","noodle","five" };
-	int phosize = 5;
-	string rtaco_bowl[] = { "beef","noodles","rice","beef" };
-	int taco_bowlsize = 4;
-	string rporkbelly[] = { "porkbelly","noodles","rice","chips","twenty" };
-	int porkbellysize = 5;
-	string rcandvstirfry[] = { "chicken","brocoli","Broccoli","Carrots","peppers","Soy sauce","garlic"};
-	int candvstirfrysize = 7;
-	string rturkeyrap[]={"turkey","lettuce","onion","hoisin sauce"};
-	int turkeyrapsaize = 4;
-
-
-	cout << "what three things have we got?" << endl;
-//taes user input and saves it as ing
-	for (int i = 0; i < size; i++)
+{
+	cout << "type new for new password or old to decrypt an old one?\n";
+	string answer;
+	cin >> answer;
+	cin.ignore(numeric_limits<streamsize>::max(), '\n'); // ignores the new line
+	if (answer == "new")
 	{
-		cin >> ing[i];
+		string password;
+		cout << "enter the password you want encrypted\n";
+		getline(cin, password);
+		encrypt(password);
 	}
-	//defines boolean match[i] and checks if any of the values in 2 arrays of 2 sizes are the same 
-	bool matchpho = anyinarray(ing, size, rpho, phosize);
-	bool matchtaco_bowl = anyinarray(ing, size, rtaco_bowl, taco_bowlsize);
-	bool matchporkbelly = anyinarray(ing, size, rporkbelly, porkbellysize);
-	bool matchcandvstirfry = anyinarray(ing, size, rcandvstirfry, candvstirfrysize);
-	bool matchturkeyrap = anyinarray(ing, size, rturkeyrap, turkeyrapsaize);
-	
-	//check if the match is true and prints a prompt pluss the ingredients 
-	if (matchpho) {
-		cout << "pho\n-------\nyoull need:\n";
-		printarray(rpho, phosize);
+	else if (answer == "old")
+	{
+		cout << "eneter your numbers for your encrypted password\n";
+		string EP;
+		getline(cin, EP);
+		int len = EP.length();
+		decrypt(EP, len);
+
 	}
-	if (matchtaco_bowl) {
-		cout << "taco bowls\n-------\nyoull need:\n";
-		printarray(rtaco_bowl, taco_bowlsize);
-	}
-	if (matchporkbelly) {
-		cout << "Pork belly\n-------\nyoull need:\n";
-		printarray(rporkbelly, porkbellysize);
-	}
-	if (matchcandvstirfry) {
-		cout << "chicken and veg stirfry\n-------\nyoull need:\n";
-		printarray(rcandvstirfry, candvstirfrysize);
-	}
-	if (matchturkeyrap) {
-		cout << "Turkey wraps\n-------\nyoull need:";
-		printarray(rturkeyrap, turkeyrapsaize);
+	else
+	{
+		return 0;
 	}
 
-
-
-
-	//print if it doesnt match any of the recipies( needs work, i dont like that i have to input it here every time)
-	if (!matchpho && !matchporkbelly && !matchtaco_bowl && !matchcandvstirfry)
-		cout << "fail";
-
-			
-	return 0;
-	
-}
+	return (0);
+};
